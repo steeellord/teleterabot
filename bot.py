7649e42db904582126cb8dde997a5273abad7662
steeellord <Pkabhisheksingh78@gmail.com>
2026-04-08 16:52:50 +0530

    bot.py

--- bot.py
+++ bot.py
@@ -1,7 +1,7 @@
 from telegram import Update
 from telegram.ext import ApplicationBuilder, MessageHandler, filters, ContextTypes
 
-BOT_TOKEN = "PUT_YOUR_BOT_TOKEN_HERE"
+BOT_TOKEN = "8326579614:AAH4×M4r@uL_nN3fsjhGkIN5TBwCQ7mFv7k"
 
 async def handle_message(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text("Bot is working!")
